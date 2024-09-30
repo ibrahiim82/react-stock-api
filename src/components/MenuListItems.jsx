1b697fbb93f912fc78ec50ba9a4208015ec73e63
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-09-30 23:10:22 +0300

    lastcommit

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -5,16 +5,15 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import AppsIcon from "@mui/icons-material/Apps";
-import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket"
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
-import InventoryIcon from "@mui/icons-material/Inventory"
-import StoreIcon from "@mui/icons-material/Store"
-import StarsIcon from "@mui/icons-material/Stars"
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
 import { useNavigate } from "react-router-dom";
 
 const MenuListItems = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const icons = [
     {
       title: "Dashboard",
@@ -22,37 +21,41 @@ const MenuListItems = () => {
       path: "/stock",
     },
     {
-        title: "Purchases",
-        icon: <ShoppingBasketIcon />,
-        path: "/stock/purchases",
-      },
-      {
-        title: "Sales",
-        icon: <AttachMoneyIcon />,
-        path: "/stock/sales/",
-      },
-      {
-        title: "Firms",
-        icon: <StoreIcon />,
-        path: "/stock/firms/",
-      },
-      {
-        title: "Brands",
-        icon: <StarsIcon />,
-        path: "/stock/brands/",
-      },
-      {
-        title: "Products",
-        icon: <InventoryIcon />,
-        path: "/stock/products/",
-      },
+      title: "Purchases",
+      icon: <ShoppingBasketIcon />,
+      path: "/stock/purchases",
+    },
+    {
+      title: "Sales",
+      icon: <AttachMoneyIcon />,
+      path: "/stock/sales/",
+    },
+    {
+      title: "Firms",
+      icon: <StoreIcon />,
+      path: "/stock/firms/",
+    },
+    {
+      title: "Brands",
+      icon: <StarsIcon />,
+      path: "/stock/brands/",
+    },
+    {
+      title: "Products",
+      icon: <InventoryIcon />,
+      path: "/stock/products/",
+    },
   ];
 
   return (
     <div>
       <List>
         {icons.map((item, index) => (
-          <ListItem key={index} disablePadding onClick={()=>navigate(item.path)} >
+          <ListItem
+            key={index}
+            disablePadding
+            onClick={() => navigate(item.path)}
+          >
             <ListItemButton>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.title} />
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import { Navigate, Outlet } from "react-router-dom"
 
 const PrivateRouter = () => {
-  const {username} = useSelector((state)=>state.auth)
+  const {username} = useSelector((state) => state.auth)
 
   return username ? <Outlet /> : <Navigate to="/" />
 }
