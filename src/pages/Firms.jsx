721bffd645e9ab2a2cbfd72ff2b9045c3946b48f
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-10-02 20:59:56 +0300

    lastcommit

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 
 const Firms = () => {
-  return (
-    <div>Firms</div>
-  )
-}
+  const {token} = useSelector((state)=>state.auth)
 
-export default Firms
+  const getFirm = async () => {
+    try {
+      const { data } = await axiosyarn.post(
+        `${process.env.REACT_APP_BASE_URL}/firms/`,{
+          headers: { "Authorization":  `Token ${token}`},
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  // sayfa yüklendikten sonra firmaları getir
+  useEffect(() => {
+    getFirm();
+  }, []);
+
+
+  return <div>Firms</div>;
+};
+
+export default Firms;
