53a28317c03018f7937da916ed286b025802883e
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-10-07 20:40:03 +0300

    lastcommit

---
+++ src/components/ProductModal.jsx
@@ -0,0 +1,75 @@
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import Modal from "@mui/material/Modal"
+import TextField from "@mui/material/TextField"
+import useStockRequests from "../services/useStockRequests"
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+}
+
+export default function ProductModal({ open, handleClose, data, setData }) {
+  const { postStock, putStock } = useStockRequests()
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (data._id) {
+      //? put
+      putStock("firms", data)
+    } else {
+      //? post
+      postStock("firms", data)
+    }
+    //? Reset form
+    setData({ image: "", address: "", phone: "", name: "" })
+    //? close modal
+    handleClose()
+  }
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            component="form"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              value={data.name}
+              onChange={handleChange}
+              required
+            />
+
+            <Button variant="contained" type="submit">
+              {data._id ? "UPDATE FIRM" : "ADD FIRM"}
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  )
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import useStockRequests from "../services/useStockRequests"
+
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import { useSelector } from "react-redux"
+import ProductModal from "../components/ProductModal"
+import ProductTable from "../components/ProductTable"
 
 const Products = () => {
+  const { getStock } = useStockRequests()
+
+  const initialState = { image: "", address: "", phone: "", name: "" }
+  const [data, setData] = useState(initialState)
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => {
+    setOpen(false)
+    setData(initialState)
+  }
+
+  useEffect(() => {
+    getStock("products")
+  }, [])
+
   return (
-    <div>Products</div>
+    <div>
+      <Typography variant="h5" color={"error"} mb={2}>
+        Products
+      </Typography>
+      <Button variant="contained" sx={{ mb: 2 }} onClick={handleOpen}>
+        NEW PRODUCT
+      </Button>
+
+      <ProductTable/>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        data={data}
+        setData={setData}
+      />
+    </div>
   )
 }
 
